3ecac3c3414a503961bbaf79b139a09af5234f09
xavier dupré <xavier.dupre@gmail.com>
2017-11-18 21:26:02 +0100

    icons

--- _doc/sphinxdoc/source/conf.py
+++ _doc/sphinxdoc/source/conf.py
@@ -3,7 +3,7 @@ import sys
 import os
 import datetime
 import re
-import sphinx_rtd_theme
+import guzzle_sphinx_theme
 
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.split(__file__)[0])))
@@ -24,13 +24,15 @@ local_template = os.path.join(os.path.abspath(
 
 from pyquickhelper.helpgen.default_conf import set_sphinx_variables, get_default_stylesheet
 set_sphinx_variables(__file__, "mlinsights", "Xavier Dupré", 2017,
-                     "sphinx_rtd_theme", [
-                         sphinx_rtd_theme.get_html_theme_path()],
+                     "guzzle_sphinx_theme", [
+                         guzzle_sphinx_theme.html_theme_path()],
                      locals(), extlinks=dict(
                          issue=('https://github.com/sdpython/mlinsights/issues/%s', 'issue')),
                      title="mlinsights", book=True)
 
 blog_root = "http://www.xavierdupre.fr/app/mlinsights/helpsphinx/"
+extensions.append("guzzle_sphinx_theme")
+html_theme_options['project_nav_name'] = 'mlinsights'
 
 html_context = {
     'css_files': get_default_stylesheet() + ['_static/my-styles.css'],
