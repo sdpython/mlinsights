6e635eb3af7f596d5305087561e89a95f417a2f6
xavier dupré <xavier.dupre@gmail.com>
2017-11-29 12:52:04 +0100

    update script

--- build_script.bat
+++ build_script.bat
@@ -1,10 +1,13 @@
+@echo off
 if "%1"=="" goto default_value_python:
 set pythonexe="%1"
 %pythonexe% setup.py write_version
 goto custom_python:
 
 :default_value_python:
-set pythonexe="c:\Python36_x64\python"
+set pythonexe="c:\Python363_x64\python"
+if not exist %pythonexe% set pythonexe="c:\Python36_x64\python"
 :custom_python:
+@echo [python] %pythonexe%
 %pythonexe% -u setup.py build_script
 if %errorlevel% neq 0 exit /b %errorlevel%
