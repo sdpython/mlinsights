9560b29d527e09680c7199acfe4305093d1480cf
xavier dupré <xavier.dupre@gmail.com>
2017-12-06 00:11:20 +0100

    change doc style

--- _doc/sphinxdoc/source/conf.py
+++ _doc/sphinxdoc/source/conf.py
@@ -3,7 +3,8 @@ import sys
 import os
 import datetime
 import re
-import guzzle_sphinx_theme
+import sphinx_rtd_theme
+import sphinx_redactor_theme
 
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.split(__file__)[0])))
@@ -24,16 +25,12 @@ local_template = os.path.join(os.path.abspath(
 
 from pyquickhelper.helpgen.default_conf import set_sphinx_variables, get_default_stylesheet
 set_sphinx_variables(__file__, "mlinsights", "Xavier Dupré", 2017,
-                     "guzzle_sphinx_theme", guzzle_sphinx_theme.html_theme_path(),
+                     "sphinx_redactor_theme", sphinx_redactor_theme.get_html_theme_path(),
                      locals(), extlinks=dict(
                          issue=('https://github.com/sdpython/mlinsights/issues/%s', 'issue')),
                      title="mlinsights", book=True)
 
 blog_root = "http://www.xavierdupre.fr/app/mlinsights/helpsphinx/"
-extensions.append("guzzle_sphinx_theme")
-html_theme_options['project_nav_name'] = 'mlinsights'
-html_theme_options['touch_icon'] = 'project_ico.ico'
-
 
 html_logo = "project_ico.png"
 
