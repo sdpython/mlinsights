bc9a9b65e91b30cf95f4831ed207c9e8d26811ae
xavier dupré <xavier.dupre@gmail.com>
2019-03-18 22:17:46 +0100

    disable import based on scikit-learn version

--- _unittests/ut_mlmodel/test_decision_tree_experiment.py
+++ _unittests/ut_mlmodel/test_decision_tree_experiment.py
@@ -27,14 +27,16 @@ except ImportError:
         sys.path.append(path)
     import src
 
-from src.mlinsights.mlmodel.piecewise_tree_regression_criterion import SimpleRegressorCriterion  # pylint: disable=E0611
-from src.mlinsights.mlmodel.piecewise_tree_regression_criterion import _test_criterion_init  # pylint: disable=E0611
-from src.mlinsights.mlmodel.piecewise_tree_regression_criterion import _test_criterion_node_impurity  # pylint: disable=E0611
-from src.mlinsights.mlmodel.piecewise_tree_regression_criterion import _test_criterion_node_impurity_children  # pylint: disable=E0611
-from src.mlinsights.mlmodel.piecewise_tree_regression_criterion import _test_criterion_update  # pylint: disable=E0611
-from src.mlinsights.mlmodel.piecewise_tree_regression_criterion import _test_criterion_node_value  # pylint: disable=E0611
-from src.mlinsights.mlmodel.piecewise_tree_regression_criterion import _test_criterion_proxy_impurity_improvement  # pylint: disable=E0611
-from src.mlinsights.mlmodel.piecewise_tree_regression_criterion import _test_criterion_impurity_improvement  # pylint: disable=E0611
+
+if compare_module_version(sklearn.__version__, "0.21") >= 0:
+    from src.mlinsights.mlmodel.piecewise_tree_regression_criterion import SimpleRegressorCriterion  # pylint: disable=E0611
+    from src.mlinsights.mlmodel.piecewise_tree_regression_criterion import _test_criterion_init  # pylint: disable=E0611
+    from src.mlinsights.mlmodel.piecewise_tree_regression_criterion import _test_criterion_node_impurity  # pylint: disable=E0611
+    from src.mlinsights.mlmodel.piecewise_tree_regression_criterion import _test_criterion_node_impurity_children  # pylint: disable=E0611
+    from src.mlinsights.mlmodel.piecewise_tree_regression_criterion import _test_criterion_update  # pylint: disable=E0611
+    from src.mlinsights.mlmodel.piecewise_tree_regression_criterion import _test_criterion_node_value  # pylint: disable=E0611
+    from src.mlinsights.mlmodel.piecewise_tree_regression_criterion import _test_criterion_proxy_impurity_improvement  # pylint: disable=E0611
+    from src.mlinsights.mlmodel.piecewise_tree_regression_criterion import _test_criterion_impurity_improvement  # pylint: disable=E0611
 
 
 class TestDecisionTreeExperiment(ExtTestCase):
