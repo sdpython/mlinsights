a8587dc8c10830b0bd359519672f68171a975e08
xavier dupré <xavier.dupre@gmail.com>
2019-04-16 00:22:24 +0200

    update documentation

--- _doc/sphinxdoc/source/conf.py
+++ _doc/sphinxdoc/source/conf.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import sys
 import os
-import sphinx_rtd_theme
+import sphinx_readable_theme
 from pyquickhelper.helpgen.default_conf import set_sphinx_variables, get_default_stylesheet
 
 
@@ -11,8 +11,7 @@ local_template = os.path.join(os.path.abspath(
     os.path.dirname(__file__)), "phdoc_templates")
 
 set_sphinx_variables(__file__, "mlinsights", "Xavier Dupré", 2019,
-                     "sphinx_rtd_theme", [
-                         sphinx_rtd_theme.get_html_theme_path()],
+                     "readable", sphinx_readable_theme.get_html_theme_path(),
                      locals(), extlinks=dict(
                          issue=('https://github.com/sdpython/mlinsights/issues/%s', 'issue')),
                      title="mlinsights", book=True)
