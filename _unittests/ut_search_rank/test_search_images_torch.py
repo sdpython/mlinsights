c1ecdde3f1448c627f0eb148c3900618a5ba8149
xavier dupré <xavier.dupre@gmail.com>
2019-04-28 15:44:34 +0200

    disable one test on circleci

--- _unittests/ut_search_rank/test_search_images_torch.py
+++ _unittests/ut_search_rank/test_search_images_torch.py
@@ -10,13 +10,14 @@ from io import StringIO
 import pandas
 import numpy
 from pyquickhelper.loghelper import fLOG
-from pyquickhelper.pycode import ExtTestCase, get_temp_folder, skipif_appveyor
+from pyquickhelper.pycode import ExtTestCase, get_temp_folder, skipif_appveyor, skipif_circleci
 from pyquickhelper.filehelper import unzip_files
 
 
 class TestSearchPredictionsImagesTorch(ExtTestCase):
 
     @skipif_appveyor("Fails due to: Tune using inter_op_parallelism_threads for best performance.")
+    @skipif_circleci("Last for ever.")
     def test_search_predictions_torch(self):
         fLOG(
             __file__,
