6f76580abb4b882da029ad251f084d1f573e2f7e
xavier dupré <xavier.dupre@gmail.com>
2019-12-20 16:59:21 +0100

    move one test to the skip list

--- _unittests/ut_module/test_SKIP_code_style.py
+++ _unittests/ut_module/test_SKIP_code_style.py
@@ -16,9 +16,8 @@ class TestCodeStyle(ExtTestCase):
         check_pep8(src_, fLOG=fLOG,
                    pylint_ignore=('C0103', 'C1801', 'R0201', 'R1705', 'W0108', 'W0613',
                                   'W0201', 'W0221', 'E0632', 'R1702', 'W0212', 'W0223',
-                                  'W0107'),
+                                  'W0107', "R1720"),
                    skip=["categories_to_integers.py:174: W0640",
-                         "R1720",
                          "E0401: Unable to import 'mlinsights.mlmodel.piecewise_tree_regression_criterion",
                          "setup.py:",
                          ])
@@ -28,10 +27,9 @@ class TestCodeStyle(ExtTestCase):
         test = os.path.normpath(os.path.join(thi, "..", ))
         check_pep8(test, fLOG=fLOG, neg_pattern="temp_.*",
                    pylint_ignore=('C0103', 'C1801', 'R0201', 'R1705', 'W0108', 'W0613',
-                                  'C0111', 'W0107', 'C0111', 'R1702', 'C0415'),
+                                  'C0111', 'W0107', 'C0111', 'R1702', 'C0415', "R1720"),
                    skip=["Instance of 'tuple' has no",
                          "[E402] module level import",
-                         "R1720",
                          "E0611: No name '_test_criterion_",
                          "E0611: No name 'SimpleRegressorCriterion'",
                          "E0611: No name 'piecewise_tree_",
