432b431d16b5b4d79e7f8762dc7a64d66c8c1d3d
xavier dupré <xavier.dupre@gmail.com>
2020-07-01 16:43:33 +0200

    pep8

--- _unittests/ut_helpers/test_parameters.py
+++ _unittests/ut_helpers/test_parameters.py
@@ -3,10 +3,6 @@
 @brief      test log(time=2s)
 """
 import unittest
-import numpy.random
-from sklearn.linear_model import LinearRegression, LogisticRegression
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
-from sklearn.pipeline import Pipeline, FeatureUnion
 from pyquickhelper.pycode import ExtTestCase
 from mlinsights.helpers.parameters import format_value
 
