cee36988db114c8f46bd4f5e142b75c00e7974c5
xavier dupré <xavier.dupre@gmail.com>
2022-01-07 14:04:58 +0100

    update for python 3.10

--- _doc/sphinxdoc/source/conf.py
+++ _doc/sphinxdoc/source/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.split(__file__)[0])))
 local_template = os.path.join(os.path.abspath(
     os.path.dirname(__file__)), "phdoc_templates")
 
-set_sphinx_variables(__file__, "mlinsights", "Xavier Dupré", 2021,
+set_sphinx_variables(__file__, "mlinsights", "Xavier Dupré", 2022,
                      "pydata_sphinx_theme", pydata_sphinx_theme.get_html_theme_path(),
                      locals(), extlinks=dict(
                          issue=('https://github.com/sdpython/mlinsights/issues/%s', 'issue')),
