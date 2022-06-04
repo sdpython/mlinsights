89d6ed946c6c9dcabf725ee45791f71f0e8e3fc3
xavier dupré <xavier.dupre@gmail.com>
2022-06-05 00:17:02 +0200

    lint

--- _unittests/ut_module/test_SKIP_code_style.py
+++ _unittests/ut_module/test_SKIP_code_style.py
@@ -14,7 +14,7 @@ class TestCodeStyle(ExtTestCase):
         thi = os.path.abspath(os.path.dirname(__file__))
         src_ = os.path.normpath(os.path.join(thi, "..", "..", "mlinsights"))
         check_pep8(src_, fLOG=fLOG,
-                   pylint_ignore=('C0103', 'C1801', 'R0201', 'R1705', 'W0108', 'W0613',
+                   pylint_ignore=('C0103', 'C1801', 'R1705', 'W0108', 'W0613',
                                   'W0201', 'W0221', 'E0632', 'R1702', 'W0212', 'W0223',
                                   'W0107', "R1720", 'R1732', 'C0209'),
                    skip=["categories_to_integers.py:174: W0640",
@@ -28,7 +28,7 @@ class TestCodeStyle(ExtTestCase):
         thi = os.path.abspath(os.path.dirname(__file__))
         test = os.path.normpath(os.path.join(thi, "..", ))
         check_pep8(test, fLOG=fLOG, neg_pattern="temp_.*",
-                   pylint_ignore=('C0103', 'C1801', 'R0201', 'R1705', 'W0108', 'W0613',
+                   pylint_ignore=('C0103', 'C1801', 'R1705', 'W0108', 'W0613',
                                   'C0111', 'W0107', 'C0111', 'R1702', 'C0415', "R1720",
                                   'R1732', 'C0209'),
                    skip=["Instance of 'tuple' has no",
