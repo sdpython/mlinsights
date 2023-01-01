29aa74ad01f81b6678f20281adc449873f432319
xavier dupré <xavier.dupre@gmail.com>
2023-01-01 14:55:34 +0100

    documentation

--- _doc/sphinxdoc/source/conf.py
+++ _doc/sphinxdoc/source/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.split(__file__)[0])))
 local_template = os.path.join(os.path.abspath(
     os.path.dirname(__file__)), "phdoc_templates")
 
-set_sphinx_variables(__file__, "mlinsights", "Xavier Dupré", 2021,
+set_sphinx_variables(__file__, "mlinsights", "Xavier Dupré", 2023,
                      "pydata_sphinx_theme", ['_static'],
                      locals(), extlinks=dict(issue=(
                          'https://github.com/sdpython/mlinsights/issues/%s',
