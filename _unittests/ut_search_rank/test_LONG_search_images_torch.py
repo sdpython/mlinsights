bdb302be6435175863704eabaee55566ac643285
xadupre <xadupre@microsoft.com>
2024-09-08 09:41:23 +0200

    warning

--- _unittests/ut_search_rank/test_LONG_search_images_torch.py
+++ _unittests/ut_search_rank/test_LONG_search_images_torch.py
@@ -6,10 +6,17 @@ from contextlib import redirect_stderr
 from io import StringIO
 import pandas
 import numpy
-from mlinsights.ext_test_case import ExtTestCase, unzip_files
+from mlinsights.ext_test_case import (
+    ExtTestCase,
+    unzip_files,
+    skipif_ci_apple,
+    ignore_warnings,
+)
 
 
 class TestSearchPredictionsImagesTorch(ExtTestCase):
+    @skipif_ci_apple("crash")
+    @ignore_warnings(UserWarning)
     def test_search_predictions_torch(self):
         from mlinsights.search_rank import SearchEnginePredictionImages
 
